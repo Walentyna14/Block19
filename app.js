cebd7b3f44b24abf1a787fdb9ec13b13864178f3
Kamila Kosmowska <kamilawkosmowska@gmail.com>
2017-04-21 15:38:18 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -10,9 +10,7 @@ var main = function(){
 }//End MAIN
 $(document).ready(main);
 //VARIABLES
-var level = 1;
-var time = 1000;
-var position;
+var position, timerStart;
 
 //FUNCTIONS
 
@@ -23,31 +21,31 @@ var randomNumber = function(min , max){
 var documentSize = function(){
 	height = $( document ).height();
 	width = $( document ).width();
+	marginX = 0.1*width;
+	marginY = 0.1*height;
 }
 
 var run = function(){
 	documentSize();
-	var marginX = 0.1*width;
 	var x = randomNumber(marginX,0.7*width-130);
-	var marginY = 0.1*height;
 	var y = randomNumber(marginY,0.7*height-135);
 	$('#item').animate({"left": x,"top": y});
 }
 var writeLevel = function(){
-	$("#current_level").text(level);
+	$("#current_level").text(level+1);
 };
 
 var levelUp = function(event) {
 	event.stopPropagation();
-
 	++level;
 	if (level==16){
 		catchItem();
 	}else{
 		writeLevel();
 		clearInterval(position);
+		time=1000-50*level;
 		position = setInterval(run, time);
-		time-=50;
+		
 		sec= sec-2;
 		
 	}
@@ -65,21 +63,25 @@ var startGame = function(){
 	$("#game_over").hide();
 	$("#game").show();
 	sec = 60;
-	min="00";
+	time = 1000;
+	level = 0;
+	clearInterval(position);
 	position = setInterval(run, time);
 	writeLevel();
-	setInterval(timer, 1000);
+	clearInterval(timerStart);
+	timerStart = setInterval(timer, 1000);
 }
-
-var catchItem = function (){
+var gameOver = function(){
 	$("#game").hide();
 	$("#game_over").show();
+};
+var catchItem = function (){
+	gameOver();
 	$("#lose").hide();
 }
 
 var notCatch = function (){
-	$("#game").hide();
-	$("#game_over").show();
+	gameOver();
 	$("#win").hide();
 }
 
@@ -89,9 +91,10 @@ var timer= function (){
 	{
 		sec = "0"+sec;
 	}
-	if (sec==0 && min==0) 
+	if (sec==0) 
 	{
 		notCatch();
 	}
+	min="00";
 	$("#timer").text(min+":"+sec);
 }
